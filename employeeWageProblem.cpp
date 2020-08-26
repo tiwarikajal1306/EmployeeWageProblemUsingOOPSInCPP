c85e8df47f9a865ca3d3eec1c3fa89053849db05
kajal <tiwarikajal1306@gmail.com>
2020-08-26 10:24:40 +0530

    ADDED daily employee wage

--- employeeWageProblem.cpp
+++ employeeWageProblem.cpp
@@ -4,15 +4,29 @@
 
 using namespace std;
 
-int main() {
+const int EMP_RATE_PER_HOUR = 20;
+
+int empHrs = 0;
+int empWage = 0;
+
+void getEmpHrs() {
+
 	srand(time(0));
-	int present = rand() % 2 +1;
-	if(present == 1){
-		cout<< "Employee is present";
-	}
-	else{
-		cout<< "Employee is absent";
-	}
+        int is_Present = rand() % 2 + 1;
+        if(is_Present == 1){
+                empHrs = 8;
+        }
+        else {
+                empHrs = 0;
+        }
+}
+
+int main() {
+
+	getEmpHrs();
+
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	cout << empWage << endl;
 return 0;
 
 }
