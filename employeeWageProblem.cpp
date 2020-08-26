4ee67e52cd32fc323c97c0c036a9325622b7dbac
kajal <tiwarikajal1306@gmail.com>
2020-08-26 10:46:59 +0530

    ADDED Employee wage for part time

--- employeeWageProblem.cpp
+++ employeeWageProblem.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 const int EMP_RATE_PER_HOUR = 20;
+const int IS_PART_TIME = 1;
+const int IS_FULL_TIME = 2;
 
 int empHrs = 0;
 int empWage = 0;
@@ -12,12 +14,19 @@ int empWage = 0;
 void getEmpHrs() {
 
 	srand(time(0));
-        int is_Present = rand() % 2 + 1;
-        if(is_Present == 1){
-                empHrs = 8;
+        int employee_Check = rand() % 3 + 1;
+        if(employee_Check == IS_PART_TIME){
+                empHrs = 4;
+		cout << "Employee wage for Part Time" << endl;
         }
+
+	else if( employee_Check == IS_FULL_TIME ) {
+		empHrs = 8;
+		cout << "Employee wage for Full Time" << endl;
+	}
         else {
                 empHrs = 0;
+		cout << "Employee is absent" << endl;
         }
 }
 
