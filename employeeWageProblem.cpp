511a52ee1cc4440a49713fd59cb53b4340251029
kajal <tiwarikajal1306@gmail.com>
2020-08-26 22:40:25 +0530

    ADDED Calculated total emp wage till condition

--- employeeWageProblem.cpp
+++ employeeWageProblem.cpp
@@ -8,15 +8,21 @@ const int EMP_RATE_PER_HOUR = 20;
 const int IS_PART_TIME = 1;
 const int IS_FULL_TIME = 2;
 const int NUMBER_OF_WORKING_DAYS = 20;
+const int MAX_HRS_IN_MONTH = 100;
 
 int empHrs = 0;
+int totalEmpHrs = 0;
 int empWage = 0;
-int totalWage = 0;
+int totalEmpWage = 0;
+int totalWorkingDays = 0;
 
-int getEmpHrs() {
-
-        int employee_Check = rand() % 3 + 1;
-	switch( employee_Check ) {
+int getEmpHrs()
+{
+	srand(time(0));
+        while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays <  NUMBER_OF_WORKING_DAYS) {
+                totalWorkingDays++;
+        	int employee_Check = rand() % 3 + 1;
+		switch( employee_Check ) {
 
 			case IS_PART_TIME:
 				empHrs = 4;
@@ -28,26 +34,25 @@ int getEmpHrs() {
 			default:
 				empHrs = 0;
 		}
-	return empHrs;
-}
 
-int getEmpWage() {
-	srand(time(0));
-
-	for( int day = 0; day < NUMBER_OF_WORKING_DAYS; day++ ) {
-		getEmpHrs();
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		totalWage += empWage;
+		totalEmpHrs += empHrs;
 	}
-	return totalWage;
+
+	return totalEmpHrs;
 }
 
-int main() {
+int getEmpWage()
+{
 
-	int workingHrs = getEmpHrs();
-	int wageForMonth = getEmpWage();
-	cout << "Total wage of month is: "<< wageForMonth << endl;
+	int totalEmpHrs = getEmpHrs();
+	totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+	return totalEmpWage;
+}
 
-return 0;
+int main()
+{
 
+	int totalEmpWage1 = getEmpWage();
+	cout << "Total wage of employee is: "<< totalEmpWage1 << endl;
+	return 0;
 }
